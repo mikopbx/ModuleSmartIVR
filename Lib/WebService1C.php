0e437cf38071abf78b95901dae83c006f2c020ed
jorikfon <nbek@miko.ru>
2021-08-25 19:03:53 +0300

    Поправил работу с TTS SpeechPro

--- Lib/WebService1C.php
+++ Lib/WebService1C.php
@@ -55,6 +55,7 @@ class WebService1C
                     curl_setopt($ch, CURLOPT_TIMEOUT, 5);
                     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
                     curl_setopt($ch, CURLOPT_POST, 1);
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, []);
                     $result = curl_exec($ch);
                     curl_close($ch);
                 } else {
