c964c715c9f3e51efb83c78e3b12c104071cbd8f
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:28:02 +0000

    Translated using Weblate (Georgian)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: MIKOPBX/ModuleSmartIVR
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulesmartivr/ka/

--- Messages/ka.php
+++ Messages/ka.php
@@ -1,1 +1,46 @@
 <?php
+return [
+    'module_smivr_lastResponsibleDuration' => 'წამების რაოდენობა, რამდენი ხანი უნდა დარეკოთ ბოლო პასუხისმგებელთან',
+    'module_smivr_lastResponsibleTime' => 'წუთების რაოდენობა, რომლებზეც CDR უნდა გაანალიზდეს პასუხისმგებლის მოსაძებნად',
+    'module_smivr_FailoverExtForeign' => 'გამოიყენება როგორც სასწრაფო დახმარების ნომერი ჭკვიანი მარშრუტიზაციის მოდულში',
+    'module_smivr_TimeoutExtForeignKey' => 'გამოიყენება როგორც ნაგულისხმევი ნომერი ჭკვიანი მარშრუტიზაციის მოდულში',
+    'module_smivr_WeGetSettingsFromCTIClient' => 'გამოყენებული იქნება საკომუნიკაციო არხი მოდულიდან "ტელეფონის პანელი 2.0 1C-ისთვის".',
+    'module_smivr_ErrorOnMakeTestIVR' => 'შეცდომა ხმოვანი მენიუს ტესტის შექმნისას',
+    'module_smivr_UpdateRecord' => 'როდესაც მოდულის სტატუსი შეიცვალა, ობიექტები განახლდა: <br>',
+    'module_smivr_UpdateStatus' => 'Სტატუსის განახლება',
+    'module_smivr_DisconnectedTTS' => 'TTS სერვისთან დაკავშირების შეცდომა',
+    'module_smivr_Disconnected1C' => 'დაკავშირების შეცდომა 1C-თან',
+    'module_smivr_Disconnected' => 'ინვალიდი',
+    'module_smivr_Connected' => 'დაკავშირებულია 1C და TTS სერვისთან',
+    'module_smivr_ValidateTimeOutExtensionNotEqualTo' => 'ნაგულისხმევი ნომერი არასწორია',
+    'module_smivr_ValidateFailOverExtensionNotEqualTo' => 'სასწრაფო დახმარების ნომერი არასწორია',
+    'module_smivr_ValidateFailOverExtension' => 'სასწრაფო დახმარების ნომერი არ არის მითითებული',
+    'module_smivr_ValidateTimeoutExtension' => 'ნაგულისხმევი ნომერი არ არის მითითებული',
+    'module_smivr_ValidateNumberOfRepeat' => 'IVR მენიუს გამეორებების რაოდენობა არასწორია',
+    'module_smivr_ValidatePubName' => '1C ვებ სერვისის პუბლიკაციის სახელი არ არის მითითებული',
+    'module_smivr_ValidateServer1CPortRange' => '1C სერვერის პორტი სწორად არ არის მითითებული',
+    'module_smivr_ValidateServer1CHostEmpty' => '1C სერვერის მისამართი არ არის შევსებული',
+    'module_smivr_EnableDebugMode' => 'მოდულის გამართვის რეჟიმის ჩართვა',
+    'module_smivr_FailoverExtension' => 'ნომერი, სადაც გამოგიგზავნით ზარს 1C და TTS კომუნიკაციის შეფერხების შემთხვევაში. მაგალითად, სტატიკური IVR მენიუ.',
+    'module_smivr_TimeoutExtension' => 'ნაგულისხმევი ნომერი',
+    'module_smivr_NumberOfRepeat' => 'IVR მენიუს გამეორებების რაოდენობა ზარამდე გადადის ნაგულისხმევ ნომერზე',
+    'module_smivr_PublicationName' => 'პუბლიკაციის სახელი',
+    'module_smivr_Password' => 'ავტორიზაციის პაროლი 1C-ში',
+    'module_smivr_Login' => 'შედით 1C ვებ სერვისზე',
+    'module_smivr_UseSSLConnection' => 'გამოიყენეთ SSL',
+    'module_smivr_Server1CHostPort' => '1C სერვერის მისამართი და პორტი',
+    'module_smivr_Library1CType' => 'ინტეგრაციის ქვესისტემის ვერსია 1C მხარეს',
+    'module_smivr_LibraryVer2NotInstalled' => 'დააინსტალირეთ ტელეფონის პანელის 2.0 მოდული 1C-სთვის, ჩართეთ და დააკონფიგურირეთ იგი',
+    'module_smivr_LibraryVer2' => 'ვერსია 2.0',
+    'module_smivr_LibraryVer1' => 'ვერსია 1.0',
+    'SubHeaderModuleSmartIVR' => 'ხმოვანი მენიუს გენერაცია ფრენის დროს CRM სისტემის მონაცემების მიხედვით',
+    'BreadcrumbModuleSmartIVR' => 'ჭკვიანი ზარის მარშრუტიზაციის მოდული (Smart IVR)',
+    'repModuleSmartIVR' => 'სმარტ მარშრუტიზაციის მოდული -% წარმოადგენს%',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 2 2019
+ */
+    'mo_ModuleSmartIVR' => 'ჭკვიანი მარშრუტიზაციის მოდული',
+];
