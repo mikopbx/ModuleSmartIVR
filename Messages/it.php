52c7ca511c85fdeb6932efbbcfa2299e9fe7e17d
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:16:51 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: MIKOPBX/ModuleSmartIVR
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulesmartivr/it/

--- Messages/it.php
+++ Messages/it.php
@@ -1,1 +1,46 @@
 <?php
+return [
+    'module_smivr_WeGetSettingsFromCTIClient' => 'Verrà utilizzato il canale di comunicazione del modulo "Pannello di telefonia 2.0 per 1C"',
+    'module_smivr_FailoverExtForeign' => 'Utilizzato come numero di emergenza nel modulo di routing intelligente',
+    'module_smivr_TimeoutExtForeignKey' => 'Utilizzato come numero predefinito nel modulo di routing intelligente',
+    'module_smivr_ErrorOnMakeTestIVR' => 'Errore durante la generazione del test del menu vocale',
+    'module_smivr_UpdateRecord' => 'Quando lo stato del modulo è cambiato, gli oggetti sono stati aggiornati: <br>',
+    'module_smivr_UpdateStatus' => 'Aggiornamento dello stato',
+    'module_smivr_DisconnectedTTS' => 'Errore durante la connessione al servizio TTS',
+    'module_smivr_Disconnected' => 'disabile',
+    'module_smivr_Connected' => 'Connesso al servizio 1C e TTS',
+    'module_smivr_ValidateTimeOutExtensionNotEqualTo' => 'Il numero predefinito non è corretto',
+    'module_smivr_ValidateFailOverExtensionNotEqualTo' => 'Numero di emergenza errato',
+    'module_smivr_ValidateFailOverExtension' => 'Numero di emergenza non specificato',
+    'module_smivr_ValidateTimeoutExtension' => 'Numero predefinito non specificato',
+    'module_smivr_ValidateNumberOfRepeat' => 'Il numero di ripetizioni del menu IVR non è corretto',
+    'module_smivr_ValidatePubName' => 'Il nome della pubblicazione del servizio web 1C non è specificato',
+    'module_smivr_ValidateServer1CPortRange' => 'La porta del server 1C non è specificata correttamente',
+    'module_smivr_ValidateServer1CHostEmpty' => 'L\'indirizzo del server 1C non è compilato',
+    'module_smivr_FailoverExtension' => 'Il numero a cui invieremo la chiamata in caso di mancata comunicazione con 1C e TTS. Ad esempio, un menu IVR statico.',
+    'module_smivr_TimeoutExtension' => 'Numero predefinito',
+    'module_smivr_NumberOfRepeat' => 'Il numero di ripetizioni del menu IVR prima che la chiamata venga trasferita al numero predefinito',
+    'module_smivr_PublicationName' => 'Nome della pubblicazione',
+    'module_smivr_Password' => 'Password per l\'autorizzazione in 1C',
+    'module_smivr_Login' => 'Accedi al servizio web 1C',
+    'module_smivr_UseSSLConnection' => 'Usa SSL',
+    'module_smivr_Server1CHostPort' => 'Indirizzo e porta del server 1C',
+    'module_smivr_Library1CType' => 'Versione del sottosistema di integrazione sul lato 1C',
+    'module_smivr_lastResponsibleDuration' => 'Numero di secondi, per quanto tempo chiamare l\'ultimo responsabile',
+    'module_smivr_lastResponsibleTime' => 'Il numero di minuti per i quali il CDR dovrebbe essere analizzato per trovare il responsabile',
+    'module_smivr_LibraryVer2NotInstalled' => 'Installa il modulo della centrale di telefonia 2.0 per 1C, abilitalo e configuralo',
+    'module_smivr_EnableDebugMode' => 'Abilita la modalità di debug del modulo',
+    'module_smivr_LibraryVer2' => 'Versione 2.0',
+    'module_smivr_LibraryVer1' => 'Versione 1.0',
+    'SubHeaderModuleSmartIVR' => 'Generazione al volo di un menu vocale in base ai dati del sistema CRM',
+    'BreadcrumbModuleSmartIVR' => 'Modulo Smart Call Routing (Smart IVR)',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 2 2019
+ */
+    'mo_ModuleSmartIVR' => 'Modulo di routing intelligente',
+    'repModuleSmartIVR' => 'Modulo di routing intelligente -% rappresenta%',
+    'module_smivr_Disconnected1C' => 'Errore di connessione con 1C',
+];
