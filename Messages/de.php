6c180ecd4a9413f56b59e7d10ac14401b69a50e7
Nikolay Beketov <nbek@miko.ru>
2021-11-19 13:50:27 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: MIKOPBX/ModuleSmartIVR
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulesmartivr/de/

--- Messages/de.php
+++ Messages/de.php
@@ -1,1 +1,46 @@
 <?php
+return [
+    'module_smivr_WeGetSettingsFromCTIClient' => 'Es wird der Kommunikationskanal aus dem Modul "Telephony Panel 2.0 for 1C" verwendet',
+    'module_smivr_FailoverExtForeign' => 'Wird als Notrufnummer im Smart Routing Modul verwendet',
+    'module_smivr_TimeoutExtForeignKey' => 'Wird als Standardnummer im Smart-Routing-Modul verwendet',
+    'module_smivr_ErrorOnMakeTestIVR' => 'Fehler bei der Testgenerierung des Sprachmenüs',
+    'module_smivr_UpdateRecord' => 'Wenn sich der Modulstatus änderte, wurden die Objekte aktualisiert: <br>',
+    'module_smivr_UpdateStatus' => 'Status update',
+    'module_smivr_DisconnectedTTS' => 'Fehler beim Verbinden mit dem TTS-Dienst',
+    'module_smivr_Connected' => 'Verbunden mit 1C- und TTS-Dienst',
+    'module_smivr_ValidateTimeOutExtensionNotEqualTo' => 'Standardnummer ist falsch',
+    'module_smivr_ValidateFailOverExtensionNotEqualTo' => 'Falsche Notrufnummer',
+    'module_smivr_ValidateFailOverExtension' => 'Notrufnummer nicht angegeben',
+    'module_smivr_ValidateTimeoutExtension' => 'Standardnummer nicht angegeben',
+    'module_smivr_ValidateNumberOfRepeat' => 'Die Anzahl der IVR-Menüwiederholungen ist falsch',
+    'module_smivr_ValidatePubName' => 'Der Veröffentlichungsname des 1C-Webdienstes ist nicht angegeben',
+    'module_smivr_ValidateServer1CPortRange' => 'Der Port des 1C-Servers ist nicht richtig angegeben',
+    'module_smivr_ValidateServer1CHostEmpty' => '1C Serveradresse ist nicht ausgefüllt',
+    'module_smivr_FailoverExtension' => 'Die Nummer, an die wir den Anruf bei Kommunikationsfehlern mit 1C und TTS senden. Zum Beispiel ein statisches IVR-Menü.',
+    'module_smivr_TimeoutExtension' => 'Standard Nebenstelle/Extension',
+    'module_smivr_NumberOfRepeat' => 'Die Anzahl der Wiederholungen des IVR-Menüs, bevor der Anruf auf die Standardnummer übertragen wird',
+    'module_smivr_PublicationName' => 'Name der Veröffentlichung',
+    'module_smivr_Password' => 'Passwort für die Autorisierung in 1C',
+    'module_smivr_Login' => 'Login für 1C Webservice',
+    'module_smivr_UseSSLConnection' => 'SSL verwenden',
+    'module_smivr_Server1CHostPort' => '1C Serveradresse und Port',
+    'module_smivr_Library1CType' => 'Version des Integrationssubsystems auf der 1C-Seite',
+    'module_smivr_lastResponsibleDuration' => 'Anzahl der Sekunden, wie lange die letzte verantwortliche Person angerufen werden soll',
+    'module_smivr_lastResponsibleTime' => 'Die Anzahl der Minuten, für die das CDR analysiert werden sollte, um den Verantwortlichen zu finden',
+    'module_smivr_LibraryVer2NotInstalled' => 'Installieren Sie das Telefonie-Panel 2.0-Modul für 1C, aktivieren und konfigurieren Sie es',
+    'module_smivr_EnableDebugMode' => 'Modul Debug-Modus aktivieren',
+    'module_smivr_LibraryVer2' => 'Version 2.0',
+    'module_smivr_LibraryVer1' => 'Version 1.0',
+    'SubHeaderModuleSmartIVR' => 'Generierung eines Sprachmenüs on the fly gemäß CRM-Systemdaten',
+    'BreadcrumbModuleSmartIVR' => 'Smart Call Routing-Modul (Smart IVR)',
+    'repModuleSmartIVR' => 'Smart Routing-Modul -% repräsentieren%',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 2 2019
+ */
+    'mo_ModuleSmartIVR' => 'Intelligentes Routing-Modul',
+    'module_smivr_Disconnected1C' => 'Verbindungsfehler mit 1C',
+    'module_smivr_Disconnected' => 'Offline',
+];
