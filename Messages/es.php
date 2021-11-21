6c8a6827d51699965e629973991c5a81a20c4191
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:19:10 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: MIKOPBX/ModuleSmartIVR
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulesmartivr/es/

--- Messages/es.php
+++ Messages/es.php
@@ -1,1 +1,46 @@
 <?php
+return [
+    'module_smivr_WeGetSettingsFromCTIClient' => 'Se utilizará el canal de comunicación del módulo "Telephony Panel 2.0 for 1C"',
+    'module_smivr_FailoverExtForeign' => 'Utilizado como número de emergencia en el módulo de enrutamiento inteligente',
+    'module_smivr_TimeoutExtForeignKey' => 'Utilizado como número predeterminado en el módulo de enrutamiento inteligente',
+    'module_smivr_ErrorOnMakeTestIVR' => 'Error durante la generación de prueba del menú de voz',
+    'module_smivr_UpdateRecord' => 'Cuando cambió el estado del módulo, los objetos se actualizaron: <br>',
+    'module_smivr_UpdateStatus' => 'Actualización de estado',
+    'module_smivr_DisconnectedTTS' => 'Error al conectarse al servicio TTS',
+    'module_smivr_Disconnected' => 'Desconectado',
+    'module_smivr_Connected' => 'Conectado al servicio 1C y TTS',
+    'module_smivr_ValidateTimeOutExtensionNotEqualTo' => 'El número predeterminado es incorrecto',
+    'module_smivr_ValidateFailOverExtensionNotEqualTo' => 'Número de emergencia incorrecto',
+    'module_smivr_ValidateFailOverExtension' => 'Número de emergencia no especificado',
+    'module_smivr_ValidateTimeoutExtension' => 'Número predeterminado no especificado',
+    'module_smivr_ValidateNumberOfRepeat' => 'El número de repeticiones del menú IVR es incorrecto',
+    'module_smivr_ValidatePubName' => 'No se especifica el nombre de publicación del servicio web 1C',
+    'module_smivr_ValidateServer1CPortRange' => 'El puerto del servidor 1C no está especificado correctamente',
+    'module_smivr_ValidateServer1CHostEmpty' => 'La dirección del servidor 1C no está completa',
+    'module_smivr_FailoverExtension' => 'El número al que enviaremos la llamada en caso de fallas de comunicación con 1C y TTS. Por ejemplo, un menú de IVR estático.',
+    'module_smivr_TimeoutExtension' => 'Número predeterminado',
+    'module_smivr_NumberOfRepeat' => 'El número de repeticiones del menú IVR antes de que la llamada se transfiera al número predeterminado.',
+    'module_smivr_PublicationName' => 'Nombre de publicación',
+    'module_smivr_Password' => 'Contraseña para autorización en 1C',
+    'module_smivr_Login' => 'Iniciar sesión para el servicio web 1C',
+    'module_smivr_UseSSLConnection' => 'Utilice SSL',
+    'module_smivr_Server1CHostPort' => 'Dirección y puerto del servidor 1C',
+    'module_smivr_Library1CType' => 'Versión del subsistema de integración en el lado 1C',
+    'module_smivr_lastResponsibleDuration' => 'Número de segundos, cuánto tiempo se debe llamar a la última persona a cargo.',
+    'module_smivr_lastResponsibleTime' => 'La cantidad de minutos por los que se debe analizar el CDR para encontrar al responsable',
+    'module_smivr_LibraryVer2NotInstalled' => 'Instale el módulo del panel de telefonía 2.0 para 1C, enciéndalo y configúrelo',
+    'module_smivr_EnableDebugMode' => 'Habilitar el modo de depuración del módulo',
+    'module_smivr_LibraryVer2' => 'Versión 2.0',
+    'module_smivr_LibraryVer1' => 'Versión 1.0',
+    'SubHeaderModuleSmartIVR' => 'Generación de un menú de voz sobre la marcha según los datos del sistema CRM',
+    'BreadcrumbModuleSmartIVR' => 'Módulo de enrutamiento de llamadas inteligente (Smart IVR)',
+    'repModuleSmartIVR' => 'Módulo de enrutamiento inteligente:% representa%',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 2 2019
+ */
+    'mo_ModuleSmartIVR' => 'Módulo de enrutamiento inteligente',
+    'module_smivr_Disconnected1C' => 'Error de conexión con 1C',
+];
