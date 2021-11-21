c951bef2ff311e5ee4dc1ff1e64af10c4b233777
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:16:47 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: MIKOPBX/ModuleSmartIVR
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulesmartivr/pt/

--- Messages/pt.php
+++ Messages/pt.php
@@ -1,1 +1,46 @@
 <?php
+return [
+    'module_smivr_WeGetSettingsFromCTIClient' => 'Será utilizado o canal de comunicação do módulo "Painel de Telefonia 2.0 para 1C"',
+    'module_smivr_FailoverExtForeign' => 'Usado como número de emergência no módulo de roteamento inteligente',
+    'module_smivr_TimeoutExtForeignKey' => 'Usado como número padrão no módulo de roteamento inteligente',
+    'module_smivr_ErrorOnMakeTestIVR' => 'Erro durante a geração de teste do menu de voz',
+    'module_smivr_UpdateRecord' => 'Quando o status do módulo mudou, os objetos foram atualizados: <br>',
+    'module_smivr_UpdateStatus' => 'Atualização de status',
+    'module_smivr_DisconnectedTTS' => 'Erro ao conectar ao serviço TTS',
+    'module_smivr_Disconnected' => 'Desconectado',
+    'module_smivr_Connected' => 'Conectado ao serviço 1C e TTS',
+    'module_smivr_ValidateTimeOutExtensionNotEqualTo' => 'O número padrão está incorreto',
+    'module_smivr_ValidateFailOverExtensionNotEqualTo' => 'Número de emergência incorreto',
+    'module_smivr_ValidateFailOverExtension' => 'Número de emergência não especificado',
+    'module_smivr_ValidateTimeoutExtension' => 'Número padrão não especificado',
+    'module_smivr_ValidateNumberOfRepeat' => 'O número de repetições do menu IVR está incorreto',
+    'module_smivr_ValidatePubName' => 'O nome da publicação do serviço da web 1C não é especificado',
+    'module_smivr_ValidateServer1CPortRange' => 'A porta do servidor 1C não foi especificada corretamente',
+    'module_smivr_ValidateServer1CHostEmpty' => 'O endereço do servidor 1C não foi preenchido',
+    'module_smivr_FailoverExtension' => 'O número para onde enviaremos a chamada em caso de falhas de comunicação com 1C e TTS. Por exemplo, um menu IVR estático.',
+    'module_smivr_TimeoutExtension' => 'Número padrão',
+    'module_smivr_NumberOfRepeat' => 'O número de repetições do menu IVR antes que a chamada seja transferida para o número padrão',
+    'module_smivr_PublicationName' => 'Nome da Publicação',
+    'module_smivr_Password' => 'Senha para autorização em 1C',
+    'module_smivr_Login' => 'Login para serviço web 1C',
+    'module_smivr_UseSSLConnection' => 'Use SSL',
+    'module_smivr_Server1CHostPort' => 'Endereço e porta do servidor 1C',
+    'module_smivr_Library1CType' => 'Versão do subsistema de integração no lado 1C',
+    'module_smivr_lastResponsibleDuration' => 'Número de segundos, quanto tempo para ligar para o último responsável',
+    'module_smivr_lastResponsibleTime' => 'O número de minutos pelos quais o CDR deve ser analisado para encontrar o responsável',
+    'module_smivr_EnableDebugMode' => 'Habilitar modo de depuração de módulo',
+    'module_smivr_LibraryVer2' => 'Versão 2.0',
+    'module_smivr_LibraryVer1' => 'Versão 1.0',
+    'SubHeaderModuleSmartIVR' => 'Geração de um menu de voz em tempo real de acordo com os dados do sistema CRM',
+    'BreadcrumbModuleSmartIVR' => 'Módulo de roteamento de chamada inteligente (IVR inteligente)',
+    'repModuleSmartIVR' => 'Módulo de roteamento inteligente -% representa%',
+    'module_smivr_LibraryVer2NotInstalled' => 'Instale o módulo do painel de telefonia 2.0 para 1C, habilite e configure-o',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 2 2019
+ */
+    'mo_ModuleSmartIVR' => 'Módulo de roteamento inteligente',
+    'module_smivr_Disconnected1C' => 'Erro de conexão com 1C',
+];
