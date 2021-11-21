0b70a2a4cbb20de5abcdf4c34a6d74585eb95cf7
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:26:51 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: MIKOPBX/ModuleSmartIVR
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulesmartivr/tr/

--- Messages/tr.php
+++ Messages/tr.php
@@ -1,1 +1,46 @@
 <?php
+return [
+    'module_smivr_WeGetSettingsFromCTIClient' => '"Telephony Panel 2.0 for 1C" modülünden gelen iletişim kanalı kullanılacaktır.',
+    'module_smivr_FailoverExtForeign' => 'Akıllı yönlendirme modülünde acil durum numarası olarak kullanılır',
+    'module_smivr_TimeoutExtForeignKey' => 'Akıllı yönlendirme modülünde varsayılan numara olarak kullanılır',
+    'module_smivr_ErrorOnMakeTestIVR' => 'Ses menüsünün test üretimi sırasında hata',
+    'module_smivr_UpdateRecord' => 'Modül durumu değiştiğinde nesneler güncellendi: <br>',
+    'module_smivr_UpdateStatus' => 'Durum Güncellemesi',
+    'module_smivr_DisconnectedTTS' => 'TTS hizmetine bağlanırken hata oluştu',
+    'module_smivr_Connected' => '1C ve TTS hizmetine bağlı',
+    'module_smivr_ValidateTimeOutExtensionNotEqualTo' => 'Varsayılan numara yanlış',
+    'module_smivr_ValidateFailOverExtensionNotEqualTo' => 'Yanlış acil durum numarası',
+    'module_smivr_ValidateFailOverExtension' => 'Acil durum numarası belirtilmedi',
+    'module_smivr_ValidateTimeoutExtension' => 'Varsayılan numara belirtilmedi',
+    'module_smivr_ValidateNumberOfRepeat' => 'IVR menü tekrarlarının sayısı yanlış',
+    'module_smivr_ValidatePubName' => '1C web hizmetinin yayın adı belirtilmedi',
+    'module_smivr_ValidateServer1CPortRange' => '1C sunucusunun bağlantı noktası doğru belirtilmemiş',
+    'module_smivr_ValidateServer1CHostEmpty' => '1C sunucu adresi doldurulmadı',
+    'module_smivr_FailoverExtension' => '1C ve TTS ile iletişim kesilmesi durumunda çağrıyı göndereceğimiz numara. Örneğin, statik bir IVR menüsü.',
+    'module_smivr_TimeoutExtension' => 'Varsayılan numara',
+    'module_smivr_NumberOfRepeat' => 'Aramanın varsayılan numaraya aktarılmasından önceki IVR menü tekrarlarının sayısı',
+    'module_smivr_PublicationName' => 'yayın adı',
+    'module_smivr_Password' => '1C\'de yetkilendirme için şifre',
+    'module_smivr_UseSSLConnection' => 'SSL kullan',
+    'module_smivr_Server1CHostPort' => '1C sunucu adresi ve bağlantı noktası',
+    'module_smivr_Library1CType' => '1C tarafında entegrasyon alt sistemi sürümü',
+    'module_smivr_LibraryVer2NotInstalled' => '1C için telefon paneli 2.0 modülünü kurun, açın ve yapılandırın',
+    'module_smivr_lastResponsibleDuration' => 'Saniye sayısı, en son sorumlu kişinin aranacağı süre',
+    'module_smivr_lastResponsibleTime' => 'Sorumluyu bulmak için CDR\'nin analiz edilmesi gereken dakika sayısı',
+    'module_smivr_EnableDebugMode' => 'Modül hata ayıklama modunu etkinleştir',
+    'module_smivr_LibraryVer2' => 'Sürüm 2.0',
+    'module_smivr_LibraryVer1' => 'Sürüm 1.0',
+    'SubHeaderModuleSmartIVR' => 'CRM sistem verilerine göre anında sesli menü oluşturma',
+    'BreadcrumbModuleSmartIVR' => 'Akıllı Çağrı Yönlendirme Modülü (Akıllı IVR)',
+    'module_smivr_Disconnected' => 'engelli',
+    'module_smivr_Disconnected1C' => '1C ile bağlantı hatası',
+    'module_smivr_Login' => '1C web hizmeti için giriş yapın',
+    'repModuleSmartIVR' => 'Akıllı Yönlendirme Modülü -% temsil eder%',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 2 2019
+ */
+    'mo_ModuleSmartIVR' => 'Akıllı yönlendirme modülü',
+];
