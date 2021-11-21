3ac53b722a6de8397709aef160fb99c46570f16d
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:21:10 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: MIKOPBX/ModuleSmartIVR
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulesmartivr/sv/

--- Messages/sv.php
+++ Messages/sv.php
@@ -1,1 +1,46 @@
 <?php
+return [
+    'module_smivr_WeGetSettingsFromCTIClient' => 'Kommunikationskanalen från modulen "Telefonipanel 2.0 för 1C" kommer att användas',
+    'module_smivr_FailoverExtForeign' => 'Används som nödnummer i smart routingmodul',
+    'module_smivr_TimeoutExtForeignKey' => 'Används som standardnummer i smart routingmodul',
+    'module_smivr_ErrorOnMakeTestIVR' => 'Fel under testgenerering av röstmenyn',
+    'module_smivr_UpdateRecord' => 'När modulens status ändrades uppdaterades objekten: <br>',
+    'module_smivr_UpdateStatus' => 'Statusuppdatering',
+    'module_smivr_DisconnectedTTS' => 'Fel vid anslutning till TTS-tjänst',
+    'module_smivr_Disconnected' => 'inaktiverad',
+    'module_smivr_Connected' => 'Ansluten till 1C och TTS-tjänst',
+    'module_smivr_ValidateTimeOutExtensionNotEqualTo' => 'Standardnumret är felaktigt',
+    'module_smivr_ValidateFailOverExtensionNotEqualTo' => 'Felaktigt nödnummer',
+    'module_smivr_ValidateFailOverExtension' => 'Nödnummer ej angivet',
+    'module_smivr_ValidateNumberOfRepeat' => 'Antalet IVR-menyupprepningar är felaktigt',
+    'module_smivr_ValidatePubName' => 'Publiceringsnamnet för 1C-webbtjänsten är inte specificerat',
+    'module_smivr_ValidateServer1CPortRange' => 'Porten för 1C-servern är inte korrekt angiven',
+    'module_smivr_ValidateServer1CHostEmpty' => '1C-serveradressen är inte ifylld',
+    'module_smivr_FailoverExtension' => 'Numret dit vi kommer att skicka samtalet vid kommunikationsfel med 1C och TTS. Till exempel en statisk IVR-meny.',
+    'module_smivr_TimeoutExtension' => 'Standardnummer',
+    'module_smivr_PublicationName' => 'Publikationens namn',
+    'module_smivr_Password' => 'Lösenord för auktorisering i 1C',
+    'module_smivr_Login' => 'Logga in för 1C webbtjänst',
+    'module_smivr_UseSSLConnection' => 'Använd SSL',
+    'module_smivr_Server1CHostPort' => '1C-serveradress och port',
+    'module_smivr_Library1CType' => 'Integrationsdelsystemversion på 1C-sidan',
+    'module_smivr_LibraryVer2NotInstalled' => 'Installera telefonipanel 2.0-modulen för 1C, slå på den och konfigurera den',
+    'module_smivr_lastResponsibleDuration' => 'Antal sekunder, hur länge ska man ringa den sista ansvarig',
+    'module_smivr_lastResponsibleTime' => 'Antalet minuter som CDR bör analyseras för att hitta den ansvarige',
+    'module_smivr_EnableDebugMode' => 'Aktivera modulfelsökningsläge',
+    'module_smivr_LibraryVer2' => 'Version 2.0',
+    'module_smivr_LibraryVer1' => 'Version 1.0',
+    'SubHeaderModuleSmartIVR' => 'Generering av en röstmeny i farten enligt CRM-systemdata',
+    'BreadcrumbModuleSmartIVR' => 'Smart Call Routing Module (Smart IVR)',
+    'module_smivr_ValidateTimeoutExtension' => 'Standardnummer inte angivet',
+    'repModuleSmartIVR' => 'Smart Routing Module - % representerar %',
+    'module_smivr_NumberOfRepeat' => 'Antalet IVR-menyupprepningar innan samtalet överförs till standardnumret',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 2 2019
+ */
+    'mo_ModuleSmartIVR' => 'Smart routingmodul',
+    'module_smivr_Disconnected1C' => 'Anslutningsfel med 1C',
+];
