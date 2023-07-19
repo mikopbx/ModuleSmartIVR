8d6557f2758ffdf14f4069cc29fee81958567f3f
Secrieru Ion <admin@yolo.md>
2023-07-19 00:05:35 +0000

    Translated using Weblate (Romanian)
    Currently translated at 2.7% (1 of 37 strings)
    
    Translation: MIKOPBX/ModuleSmartIVR
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulesmartivr/ro/

--- Messages/ro.php
+++ Messages/ro.php
@@ -1,1 +1,10 @@
 <?php
+return [
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 2 2019
+ */
+    'mo_ModuleSmartIVR' => 'Modul de rutare inteligentă',
+];
