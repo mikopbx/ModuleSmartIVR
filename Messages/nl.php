805f4df8eabd29781ee307c797f4bd6ae9999afd
Anonymous <noreply@weblate.org>
2023-09-01 12:01:31 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: MIKOPBX/ModuleSmartIVR
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulesmartivr/nl/

--- Messages/nl.php
+++ Messages/nl.php
@@ -1,1 +1,46 @@
 <?php
+return [
+    'repModuleSmartIVR' => 'Slimme routeringsmodule - %represent%',
+    'SubHeaderModuleSmartIVR' => 'On-the-fly generatie van spraakmenu\'s op basis van CRM-systeemgegevens',
+    'module_smivr_LibraryVer1' => 'Versie 1.0',
+    'module_smivr_LibraryVer2' => 'Versie 2.0',
+    'module_smivr_LibraryVer2NotInstalled' => 'Installeer de telefoniepaneel 2.0-module voor 1C, schakel deze in en configureer deze',
+    'module_smivr_Library1CType' => 'Versie van het integratiesubsysteem aan de 1C-zijde',
+    'module_smivr_Server1CHostPort' => 'Serveradres en poort 1C',
+    'module_smivr_UseSSLConnection' => 'Gebruik SSL',
+    'module_smivr_Login' => 'Inloggen voor webservice 1C',
+    'module_smivr_NumberOfRepeat' => 'Aantal herhalingen van het IVR-menu vóór het doorverbinden van het standaardnummer',
+    'module_smivr_ValidateServer1CHostEmpty' => '1C serveradres niet ingevuld',
+    'module_smivr_ValidateServer1CPortRange' => 'De poort van de 1C-server is niet correct opgegeven',
+    'module_smivr_ValidatePubName' => 'De naam van de publicatie van de 1C-webservice is niet gespecificeerd',
+    'module_smivr_ValidateNumberOfRepeat' => 'Het aantal herhalingen van het IVR-menu is onjuist',
+    'module_smivr_ValidateTimeoutExtension' => 'Geen standaardnummer opgegeven',
+    'module_smivr_ValidateFailOverExtension' => 'Er is geen noodnummer opgegeven',
+    'module_smivr_ValidateFailOverExtensionNotEqualTo' => 'Ongeldig noodnummer',
+    'module_smivr_ValidateTimeOutExtensionNotEqualTo' => 'Ongeldig standaardnummer',
+    'module_smivr_Connected' => 'Verbonden met 1C en TTS-service',
+    'module_smivr_Disconnected' => 'Gehandicapt',
+    'module_smivr_Disconnected1C' => 'Verbindingsfout met 1C',
+    'module_smivr_DisconnectedTTS' => 'Fout bij het verbinden met de TTS-service',
+    'module_smivr_UpdateStatus' => 'Status update',
+    'module_smivr_ErrorOnMakeTestIVR' => 'Fout tijdens het testen van het spraakmenu',
+    'module_smivr_WeGetSettingsFromCTIClient' => 'Er wordt gebruik gemaakt van het communicatiekanaal van de module "Telefoniepaneel 2.0 voor 1C".',
+    'module_smivr_TimeoutExtForeignKey' => 'Gebruikt als standaardnummer in de slimme routeringsmodule',
+    'module_smivr_lastResponsibleTime' => 'Het aantal minuten dat de CDR moet worden geanalyseerd om de eigenaar te vinden',
+    'module_smivr_lastResponsibleDuration' => 'Aantal seconden, hoe lang het duurt om de laatste verantwoordelijke persoon te bellen',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 2 2019
+ */
+    'mo_ModuleSmartIVR' => 'Slimme routeringsmodule',
+    'BreadcrumbModuleSmartIVR' => 'Slimme oproeprouteringsmodule (Smart IVR)',
+    'module_smivr_Password' => 'Wachtwoord voor autorisatie in 1C',
+    'module_smivr_PublicationName' => 'Publicatienaam',
+    'module_smivr_TimeoutExtension' => 'Standaardnummer',
+    'module_smivr_FailoverExtension' => 'Het nummer waarnaar we zullen bellen in geval van communicatiestoringen met 1C en TTS. Bijvoorbeeld een statisch IVR-menu.',
+    'module_smivr_EnableDebugMode' => 'Schakel de foutopsporingsmodus voor de module in',
+    'module_smivr_UpdateRecord' => 'Objecten bijgewerkt wanneer de modulestatus veranderde:<br>',
+    'module_smivr_FailoverExtForeign' => 'Gebruikt als noodnummer in de slimme routeringsmodule',
+];
