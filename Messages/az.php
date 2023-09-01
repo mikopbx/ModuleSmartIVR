f79a46e638d1dc24fbda708d50f5b6254499e50b
Anonymous <noreply@weblate.org>
2023-09-01 12:01:45 +0000

    Translated using Weblate (Azerbaijani)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: MIKOPBX/ModuleSmartIVR
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulesmartivr/az/

--- Messages/az.php
+++ Messages/az.php
@@ -1,1 +1,46 @@
 <?php
+return [
+    'repModuleSmartIVR' => 'Ağıllı Marşrutlaşdırma Modulu - %present%',
+    'BreadcrumbModuleSmartIVR' => 'Ağıllı Zəng Yönləndirmə Modulu (Smart IVR)',
+    'SubHeaderModuleSmartIVR' => 'CRM sistem məlumatlarına uyğun olaraq səsli menyunun yaradılması',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 2 2019
+ */
+    'mo_ModuleSmartIVR' => 'Ağıllı marşrutlaşdırma modulu',
+    'module_smivr_LibraryVer1' => 'Versiya 1.0',
+    'module_smivr_ValidateFailOverExtension' => 'Təcili yardım nömrəsi verilməyib',
+    'module_smivr_ValidateFailOverExtensionNotEqualTo' => 'Yanlış təcili yardım nömrəsi',
+    'module_smivr_ValidateTimeOutExtensionNotEqualTo' => 'Yanlış defolt nömrə',
+    'module_smivr_Connected' => '1C və TTS xidmətinə qoşulub',
+    'module_smivr_TimeoutExtForeignKey' => 'Ağıllı marşrutlaşdırma modulunda standart nömrə kimi istifadə olunur',
+    'module_smivr_FailoverExtForeign' => 'Ağıllı marşrutlaşdırma modulunda təcili yardım nömrəsi kimi istifadə olunur',
+    'module_smivr_lastResponsibleDuration' => 'Saniyələrin sayı, son məsul şəxsə nə qədər zəng etmək lazımdır',
+    'module_smivr_LibraryVer2' => 'Versiya 2.0',
+    'module_smivr_LibraryVer2NotInstalled' => '1C üçün telefon paneli 2.0 modulunu quraşdırın, onu işə salın və konfiqurasiya edin',
+    'module_smivr_Library1CType' => '1C tərəfində inteqrasiya altsisteminin versiyası',
+    'module_smivr_Server1CHostPort' => 'Server ünvanı və port 1C',
+    'module_smivr_UseSSLConnection' => 'SSL istifadə edin',
+    'module_smivr_Login' => '1C veb xidmətinə daxil olun',
+    'module_smivr_Password' => '1c-də avtorizasiya üçün parol',
+    'module_smivr_PublicationName' => 'Nəşrin adı',
+    'module_smivr_NumberOfRepeat' => 'Zəng köçürməzdən əvvəl IVR menyusunun təkrar sayı',
+    'module_smivr_Disconnected' => 'Əlil',
+    'module_smivr_Disconnected1C' => '1C ilə əlaqə xətası',
+    'module_smivr_TimeoutExtension' => 'Defolt nömrə',
+    'module_smivr_FailoverExtension' => '1C və TTS ilə əlaqə kəsildikdə zəngi göndərəcəyimiz nömrə. Məsələn, statik IVR menyusu.',
+    'module_smivr_EnableDebugMode' => 'Modul sazlama rejimini aktivləşdirin',
+    'module_smivr_ValidateServer1CHostEmpty' => '1C server ünvanı doldurulmayıb',
+    'module_smivr_DisconnectedTTS' => 'TTS xidmətinə qoşulma xətası',
+    'module_smivr_ValidateServer1CPortRange' => '1C serverinin portu düzgün göstərilməyib',
+    'module_smivr_ValidatePubName' => '1C veb xidmətinin nəşrinin adı göstərilmir',
+    'module_smivr_ValidateNumberOfRepeat' => 'IVR menyusunun təkrar sayı səhvdir',
+    'module_smivr_ValidateTimeoutExtension' => 'Defolt nömrə göstərilməyib',
+    'module_smivr_UpdateStatus' => 'Status yeniləməsi',
+    'module_smivr_UpdateRecord' => 'Modul statusu dəyişdikdə yenilənən obyektlər:<br>',
+    'module_smivr_ErrorOnMakeTestIVR' => 'Səs menyusunun sınaq yaradılması zamanı xəta',
+    'module_smivr_WeGetSettingsFromCTIClient' => '“Telefoniya Paneli 2.0 1C üçün” modulundan rabitə kanalı istifadə olunacaq',
+    'module_smivr_lastResponsibleTime' => 'Sahibi tapmaq üçün CDR-ni təhlil etmək üçün dəqiqələrin sayı',
+];
