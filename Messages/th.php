1a5f8a0ac7eca238bb95bff554faaf417778d286
Anonymous <noreply@weblate.org>
2024-02-23 02:57:09 +0000

    Translated using Weblate (Thai)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: MIKOPBX/ModuleSmartIVR
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulesmartivr/th/

--- Messages/th.php
+++ Messages/th.php
@@ -1,1 +1,46 @@
 <?php
+return [
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 2 2019
+ */
+    'mo_ModuleSmartIVR' => 'โมดูลการกำหนดเส้นทางอัจฉริยะ',
+    'module_smivr_Disconnected' => 'พิการ',
+    'module_smivr_lastResponsibleTime' => 'จำนวนนาทีที่ควรวิเคราะห์ CDR เพื่อค้นหาผู้รับผิดชอบ',
+    'repModuleSmartIVR' => 'โมดูลการกำหนดเส้นทางอัจฉริยะ - % เป็นตัวแทน%',
+    'BreadcrumbModuleSmartIVR' => 'โมดูลกำหนดเส้นทางการโทรอัจฉริยะ (Smart IVR)',
+    'SubHeaderModuleSmartIVR' => 'การสร้างเมนูเสียงได้ทันทีตามข้อมูลระบบ CRM',
+    'module_smivr_LibraryVer1' => 'เวอร์ชัน 1.0',
+    'module_smivr_LibraryVer2' => 'เวอร์ชัน 2.0/4.0',
+    'module_smivr_LibraryVer2NotInstalled' => 'ติดตั้งโมดูลแผงโทรศัพท์ 4.0 สำหรับ 1C เปิดใช้งานและกำหนดค่า',
+    'module_smivr_Library1CType' => 'เวอร์ชันของระบบย่อยการรวมทางฝั่ง 1C',
+    'module_smivr_Server1CHostPort' => 'ที่อยู่เซิร์ฟเวอร์ 1C และพอร์ต',
+    'module_smivr_UseSSLConnection' => 'ใช้ SSL',
+    'module_smivr_Login' => 'เข้าสู่ระบบบริการเว็บ 1C',
+    'module_smivr_Password' => 'รหัสผ่านสำหรับการอนุญาตใน 1C',
+    'module_smivr_PublicationName' => 'ชื่อสิ่งพิมพ์',
+    'module_smivr_NumberOfRepeat' => 'จำนวนการซ้ำเมนู IVR ก่อนโอนสายไปยังหมายเลขเริ่มต้น',
+    'module_smivr_TimeoutExtension' => 'หมายเลขเริ่มต้น',
+    'module_smivr_FailoverExtension' => 'หมายเลขที่เราจะส่งสายในกรณีที่การสื่อสารล้มเหลวกับ 1C และ TTS เช่น เมนู IVR แบบคงที่',
+    'module_smivr_EnableDebugMode' => 'เปิดใช้งานโหมดแก้ไขข้อบกพร่องของโมดูล',
+    'module_smivr_ValidateServer1CHostEmpty' => 'ไม่ได้กรอกที่อยู่เซิร์ฟเวอร์ 1C',
+    'module_smivr_ValidateServer1CPortRange' => 'ระบุพอร์ตเซิร์ฟเวอร์ 1C ไม่ถูกต้อง',
+    'module_smivr_ValidatePubName' => 'ไม่ได้ระบุชื่อการเผยแพร่ของบริการเว็บ 1C',
+    'module_smivr_ValidateNumberOfRepeat' => 'จำนวนการทำซ้ำเมนู IVR ไม่ถูกต้อง',
+    'module_smivr_ValidateTimeoutExtension' => 'ไม่ได้ระบุหมายเลขเริ่มต้น',
+    'module_smivr_ValidateFailOverExtension' => 'ไม่ได้ระบุหมายเลขฉุกเฉิน',
+    'module_smivr_ValidateFailOverExtensionNotEqualTo' => 'หมายเลขฉุกเฉินไม่ถูกต้อง',
+    'module_smivr_ValidateTimeOutExtensionNotEqualTo' => 'หมายเลขเริ่มต้นไม่ถูกต้อง',
+    'module_smivr_Connected' => 'เชื่อมต่อกับบริการ 1C และ TTS',
+    'module_smivr_Disconnected1C' => 'ข้อผิดพลาดในการเชื่อมต่อกับ 1C',
+    'module_smivr_DisconnectedTTS' => 'เกิดข้อผิดพลาดในการเชื่อมต่อกับบริการ TTS',
+    'module_smivr_UpdateStatus' => 'อัพเดทสถานะ',
+    'module_smivr_UpdateRecord' => 'เมื่อสถานะของโมดูลเปลี่ยนไป ออบเจ็กต์ได้รับการอัปเดต:<br>',
+    'module_smivr_ErrorOnMakeTestIVR' => 'เกิดข้อผิดพลาดระหว่างการทดสอบการสร้างเมนูเสียง',
+    'module_smivr_WeGetSettingsFromCTIClient' => 'จะใช้ช่องทางการสื่อสารจากโมดูล "แผงโทรศัพท์ 4.0 สำหรับ 1C"',
+    'module_smivr_TimeoutExtForeignKey' => 'ใช้เป็นหมายเลขเริ่มต้นในโมดูลเส้นทางอัจฉริยะ',
+    'module_smivr_FailoverExtForeign' => 'ใช้เป็นหมายเลขฉุกเฉินในโมดูลเส้นทางอัจฉริยะ',
+    'module_smivr_lastResponsibleDuration' => 'จำนวนวินาทีระยะเวลาในการโทรหาผู้รับผิดชอบคนสุดท้าย',
+];
