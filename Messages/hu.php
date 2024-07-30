c5b43e02c85720d5149c95bbd2c8b99ff26f4dc1
Juhász Béla <korfa@freemail.hu>
2024-07-30 12:41:22 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 32.4% (12 of 37 strings)
    
    Translation: MIKOPBX/ModuleSmartIVR
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulesmartivr/hu/

--- Messages/hu.php
+++ Messages/hu.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    'BreadcrumbModuleSmartIVR' => 'Intelligens hívásirányító modul (Smart IVR)',
+    'module_smivr_LibraryVer1' => '1.0 verzió',
+    'module_smivr_LibraryVer2NotInstalled' => 'Telepítse a Telephony Panel 4.0 modult az 1C számára, engedélyezze és konfigurálja azt',
+    'module_smivr_Library1CType' => 'Az integrációs alrendszer változata az 1C oldalon',
+    'module_smivr_Password' => 'Jelszó az 1C-ben történő engedélyezéshez',
+    'module_smivr_NumberOfRepeat' => 'Az IVR menü ismétléseinek száma a hívás alapértelmezett számra történő átirányítása előtt',
+    'SubHeaderModuleSmartIVR' => 'On-the-fly hangmenü generálás a CRM rendszer adatai alapján',
+    'module_smivr_LibraryVer2' => '2.0/4.0 verzió',
+    'module_smivr_Server1CHostPort' => 'Az 1C szerver címe és portja',
+    'module_smivr_UseSSLConnection' => 'SSL használata',
+    'module_smivr_Login' => 'Bejelentkezés az 1C webes szolgáltatáshoz',
+    'module_smivr_PublicationName' => 'A kiadvány neve',
+];
