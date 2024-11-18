938a0d5a0098afd3edd9ceeea3fc9aa249a61def
Anonymous <noreply@weblate.org>
2024-11-18 23:05:27 +0000

    Translated using Weblate (Croatian)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: MIKOPBX/ModuleSmartIVR
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/modulesmartivr/hr/

--- Messages/hr.php
+++ Messages/hr.php
@@ -1,1 +1,46 @@
 <?php
+return [
+    'module_smivr_Connected' => 'Povezan na uslugu 1C i TTS',
+    'module_smivr_FailoverExtForeign' => 'Koristi se kao broj za hitne slučajeve u modulu pametnog usmjeravanja',
+    'module_smivr_lastResponsibleTime' => 'Broj minuta za koje CDR treba analizirati da bi se pronašla odgovorna osoba',
+    'module_smivr_lastResponsibleDuration' => 'Broj sekundi koliko dugo treba nazvati posljednju zaduženu osobu',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 2 2019
+ */
+    'mo_ModuleSmartIVR' => 'Modul pametnog usmjeravanja',
+    'repModuleSmartIVR' => 'Modul pametnog usmjeravanja - %represent%',
+    'BreadcrumbModuleSmartIVR' => 'Modul za pametno usmjeravanje poziva (Smart IVR)',
+    'SubHeaderModuleSmartIVR' => 'Generiranje glasovnog izbornika u hodu prema podacima CRM sustava',
+    'module_smivr_LibraryVer1' => 'Verzija 1.0',
+    'module_smivr_LibraryVer2' => 'Verzija 2.0/4.0',
+    'module_smivr_LibraryVer2NotInstalled' => 'Instalirajte modul telefonske ploče 4.0 za 1C, omogućite ga i konfigurirajte',
+    'module_smivr_Library1CType' => 'Verzija integracijskog podsustava na strani 1C',
+    'module_smivr_Server1CHostPort' => 'Adresa i port 1C poslužitelja',
+    'module_smivr_UseSSLConnection' => 'Koristite SSL',
+    'module_smivr_Login' => 'Prijava za web uslugu 1C',
+    'module_smivr_Password' => 'Lozinka za autorizaciju u 1C',
+    'module_smivr_PublicationName' => 'Naziv publikacije',
+    'module_smivr_NumberOfRepeat' => 'Broj ponavljanja IVR izbornika prije prijenosa poziva na zadani broj',
+    'module_smivr_TimeoutExtension' => 'Zadani broj',
+    'module_smivr_FailoverExtension' => 'Broj na koji ćemo poslati poziv u slučaju kvara komunikacije s 1C i TTS. Na primjer, statični IVR izbornik.',
+    'module_smivr_EnableDebugMode' => 'Omogući način otklanjanja pogrešaka modula',
+    'module_smivr_ValidateServer1CHostEmpty' => 'Adresa poslužitelja 1C nije popunjena',
+    'module_smivr_ValidateServer1CPortRange' => 'Port poslužitelja 1C je pogrešno naveden',
+    'module_smivr_ValidatePubName' => 'Naziv publikacije web usluge 1C nije naveden',
+    'module_smivr_ValidateNumberOfRepeat' => 'Broj ponavljanja IVR izbornika nije točan',
+    'module_smivr_ValidateTimeoutExtension' => 'Zadani broj nije naveden',
+    'module_smivr_ValidateFailOverExtension' => 'Broj hitne službe nije naveden',
+    'module_smivr_ValidateFailOverExtensionNotEqualTo' => 'Netočan broj za hitne slučajeve',
+    'module_smivr_ValidateTimeOutExtensionNotEqualTo' => 'Zadani broj nije točan',
+    'module_smivr_Disconnected' => 'Onesposobljeno',
+    'module_smivr_Disconnected1C' => 'Pogreška veze s 1C',
+    'module_smivr_DisconnectedTTS' => 'Pogreška pri povezivanju s TTS uslugom',
+    'module_smivr_UpdateStatus' => 'Ažuriranje statusa',
+    'module_smivr_UpdateRecord' => 'Kada se status modula promijenio, objekti su ažurirani:<br>',
+    'module_smivr_ErrorOnMakeTestIVR' => 'Pogreška tijekom testnog generiranja glasovnog izbornika',
+    'module_smivr_WeGetSettingsFromCTIClient' => 'Koristit će se komunikacijski kanal iz modula "Telephony Panel 4.0 for 1C".',
+    'module_smivr_TimeoutExtForeignKey' => 'Koristi se kao zadani broj u modulu pametnog usmjeravanja',
+];
